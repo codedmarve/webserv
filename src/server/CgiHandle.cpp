2e6bf00c1402912da3804804ea286355d0ff200e
Alexandros Lappas <alappas@c2r9s7.42wolfsburg.de>
2024-04-10 03:11:13 +0200

    CGI template first completion

--- src/server/CgiHandle.cpp
+++ src/server/CgiHandle.cpp
@@ -3,21 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   CgiHandle.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: drey <drey@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: alappas <alappas@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 01:28:35 by alappas           #+#    #+#             */
-/*   Updated: 2024/03/22 12:46:31 by drey             ###   ########.fr       */
+/*   Updated: 2024/04/10 03:10:13 by alappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/CgiHandle.hpp"
+#include "../../inc/Test.hpp"
 
-CgiHandle::CgiHandle()
-: _cgi_path(NULL), _cgi_pid(-1), _exit_status(0){}
+// CgiHandle::CgiHandle()
+// : _cgi_path(NULL), _cgi_pid(-1), _exit_status(0){}
+
+CgiHandle::CgiHandle(RequestConfig *config)
+: _config(config), _cgi_path(""), _cgi_pid(-1), _exit_status(0), _argv(NULL), _envp(NULL), _path(NULL){
+	this->initEnv();
+	
+}
 
 CgiHandle::~CgiHandle(){
-	delete [] this->_argv;
-	delete [] this->_envp;
+	if (this->_argv)
+	{
+		for (int i = 0; this->_argv[i]; i++)
+			free(this->_argv[i]);
+		delete[] this->_argv;
+	}
+	if (this->_envp)
+	{
+		for (int i = 0; this->_envp[i]; i++)
+			free(this->_envp[i]);
+		delete[] this->_envp;
+	}
+	if (this->_path)
+		free(this->_path);
 }
 
 CgiHandle::CgiHandle(const CgiHandle &other)
@@ -34,27 +52,30 @@ CgiHandle& CgiHandle::operator=(const CgiHandle &other){
 
 void CgiHandle::initEnv(){
 
-	std::string cgiExec = "/cgi-bin/hello.sh";
+	std::stringstream ss;
 	
-	this->_env["CONTENT_TYPE"] = "";
+	this->_env["CONTENT_TYPE"] = this->_config->getHeader("content-type");
 	this->_env["GATEWAY_INTERFACE"] = "CGI/1.1";
-	this->_env["REMOTE_ADDR"] = "";
-	this->_env["QUERY_STRING"] = "";
-	this->_env["REMOTE_PORT"] = "";
-	this->_env["CONTENT_LENGTH"] = "";
-	this->_env["REQUEST_URI"] = "";
+	this->_env["REMOTE_ADDR"] = getIp();
+	this->_env["QUERY_STRING"] = this->_config->getQuery();
+	this->_env["REMOTE_PORT"] = this->_config->getPort();
+	ss << this->_config->getContentLength();
+	this->_env["CONTENT_LENGTH"] = ss.str();
+	ss.clear();
+	this->_env["REQUEST_URI"] = this->_config->getUri();
 	this->_env["SERVER_SOFTWARE"] = "AMANIX";
-	this->_env["PATH"] = "";
-	this->_env["HTTP_REFERER"] = "";
-	this->_env["SERVER_PROTOCOL"] = "";
-	this->_env["PATH_INFO"] = "";
-	this->_env["REQUEST_METHOD"] = "";
-	this->_env["PWD"] = "";
-	this->_env["SERVER_PORT"] = "8002";
-	this->_env["SCRIPT_NAME"] = "";
-	this->_env["REMOTE_USER"] = "";
+	this->_env["PATH"] = std::string (getenv("PATH"));
+	this->_env["SERVER_PROTOCOL"] = this->_config->getProtocol();
+	this->_env["REQUEST_METHOD"] = this->_config->getMethod();
+	this->_env["PWD"] = std::string (getenv("PWD"));
+	ss << this->_config->getPort();
+	this->_env["HTTP_REFERER"] = "http://" + this->_config->getHost() + ":" + ss.str() + this->_config->getUri();
+	this->_env["SERVER_PORT"] = ss.str();
+	this->_env["SCRIPT_NAME"] = this->_config->getUri();
 	this->_env["REDIRECT_STATUS"] = "200";
 	this->_env["SERVER_NAME"] = "localhost";//getHeader?
+	// this->_env["REMOTE_USER"] = "";
+	// this->_env["PATH_INFO"] = "";
 }
 
 void CgiHandle::createEnvArray(){
@@ -67,14 +88,13 @@ void CgiHandle::createEnvArray(){
 		i++;
 	}
 	this->_argv[i] = NULL;
-
 }
 
 void CgiHandle::execCgi(){
 	
-	std::string cgiExec = "/cgi-bin/hello.sh";
-	this->_cgi_path = getenv("PWD") + cgiExec;
-	
+	this->setPath();
+	this->setArgv();
+	this->createEnvArray();
 	if (this->_argv[0] == NULL || this->_argv[1] == NULL)
 	{
 		std::cerr << "Error: invalid arguments" << std::endl;
@@ -97,7 +117,7 @@ void CgiHandle::execCgi(){
 		dup2(this->pipe_in[0], STDIN_FILENO);
 		dup2(this->pipe_out[1], STDOUT_FILENO);
 		closePipe();
-		_exit_status = execve(this->_cgi_path.c_str(), this->_argv, this->_envp);
+		_exit_status = execve(this->_path, this->_argv, this->_envp);
 		exit(_exit_status);
 	}
 }
@@ -116,4 +136,23 @@ void CgiHandle::closePipe(){
 	close(this->pipe_in[1]);
 	close(this->pipe_out[0]);
 	close(this->pipe_out[1]);
+}
+
+std::string CgiHandle::getIp(){
+	std::string full_ip = this->_config->getHost();
+	std::stringstream ss;
+	std::getline(ss, full_ip, ':');
+	return full_ip;
+}
+
+void CgiHandle::setPath(){
+	std::string path(getenv("PWD"));
+	path += this->_config->getUri();
+	this->_path = strdup(path.c_str());
+}
+
+void CgiHandle::setArgv(){
+	this->_argv = new char*[2];
+	this->_argv[0] = strdup(this->_path);
+	this->_argv[1] = NULL;
 }
