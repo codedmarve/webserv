9610ba88fc68c441117e4f061007aaff5680daba
doduwole <doduwole@student.42wolfsburg.de>
2024-05-19 13:02:59 +0200

    changed serverDB to json and also added serverDB to config file

--- www/scripts/upload.py
+++ www/scripts/upload.py
@@ -3,6 +3,7 @@
 import cgi
 import os
 import uuid
+import json
 
 def handle_file_upload(form, current_directory):
     if 'uploadFile' in form and form['uploadFile'].filename:
@@ -10,47 +11,41 @@ def handle_file_upload(form, current_directory):
         
         filename_base = os.path.basename(fileitem.filename)
         
-        unique_folder_name = str(uuid.uuid4())  # Generate a UUID
+        unique_folder_name = str(uuid.uuid4())
         submission_directory = os.path.join(current_directory, 'serverDB', unique_folder_name)
         os.makedirs(submission_directory, exist_ok=True)
 
         save_path = os.path.join(submission_directory, filename_base)
-        
-        # Open and write the file
+
         with open(save_path, 'wb') as f:
             f.write(fileitem.file.read())
         
-        message = f'The file "{filename_base}" was uploaded to {save_path}'
-        return submission_directory, message
+        return submission_directory, f'The file "{filename_base}" was uploaded to {save_path}'
     else:
         return None, 'No file uploaded or field name "uploadFile" is missing'
 
-def save_form_fields(form, submission_directory):
-    text_file_path = os.path.join(submission_directory, 'submission_details.txt')
-    with open(text_file_path, 'w') as text_file:
-        for key in form.keys():
-            if key != 'uploadFile':  # Exclude the uploaded file from the text file
-                # Write each form field and its value to the text file
-                text_file.write(f"{key}: {form.getvalue(key)}\n")
-    return text_file_path
+def save_form_fields_as_json(form, submission_directory):
+    json_file_path = os.path.join(submission_directory, 'submission_details.json')
+    form_data = {key: form.getvalue(key) for key in form.keys() if key != 'uploadFile'}
+    
+    with open(json_file_path, 'w') as json_file:
+        json.dump(form_data, json_file, indent=4)
+    
+    return json_file_path
 
-# Create instance of FieldStorage
 form = cgi.FieldStorage()
 
-# Get current directory
 current_directory = os.getcwd() + '/www'
 
-# Handle file upload
-submission_directory, message = handle_file_upload(form, current_directory)
+submission_directory, _ = handle_file_upload(form, current_directory)
 
-# Save form fields
 if submission_directory:
-    text_file_path = save_form_fields(form, submission_directory)
-    message += f'<br>Submission details saved in: {text_file_path}'
+    save_form_fields_as_json(form, submission_directory)
 
-# Output the message with appropriate HTTP headers
-print("Content-Type: text/html;charset=utf-8")
+response = {
+    "submission_directory": submission_directory if submission_directory else None
+}
+
+print("Content-Type: application/json;charset=utf-8")
 print()
-print("<html><body>")
-print(f"<h1>{message}</h1>")
-print("</body></html>")
+print(json.dumps(response, indent=4))
